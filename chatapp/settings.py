07fc7491a8efe87c31d951a8384ebec6fbe668a3
Bot-discord-hosting <111009520+Bot-discord-hosting@users.noreply.github.com>
2023-03-26 01:23:26 +0100

    Update settings.py

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b8h+j9bzb_@$-61!cuslh+-!-h(*s%#%eqspzd@wqy$x#t=5ov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "*",
